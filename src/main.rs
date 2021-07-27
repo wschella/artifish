2235e2c1aea0c5defe534a22411384fabb43f30c
Ilion Beyst <ilion@dataroots.io>
2021-07-27 22:06:16 +0200

    moving fishes

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 use rand::Rng;
+use decorum::NotNan;
 
 const MAX_X: f64 = 800.0;
 const MAX_Y: f64 = 600.0;
@@ -24,10 +25,144 @@ pub struct State {
     fishes: Vec<Fish>,
 }
 
+#[derive(Clone, Copy)]
+pub struct Vec2 {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Vec2 {
+    fn new(x: f64, y: f64) -> Self {
+        Vec2 { x, y }
+    }
+
+    fn length(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+
+    fn invert(&self) -> Vec2 {
+        Vec2 {
+            x: -self.x,
+            y: -self.y,
+        }
+    }
+}
+
+impl std::ops::Div<f64> for Vec2 {
+    type Output = Vec2;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec2 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Vec2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec2 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        let mut fishes = Vec::new();
+        let mut rng = rand::thread_rng();
+    
+        for _ in 0..100 {
+            let x = rng.gen_range(0.0..600.0);
+            let y = rng.gen_range(0.0..600.0);
+            let radius = rng.gen_range(5.0..1000.0);
+    
+            let fish = Fish::new(x, y, radius);
+            fishes.push(fish);
+        }
+
+        Self { fishes }
+    }
+
+    fn update(&mut self, delta_time: f64) {
+        for fish in self.fishes.iter_mut() {
+            fish.energy += 1.0 * fish.surface_area() * delta_time;
+        }
+
+        for i in 0..self.fishes.len() {
+            if self.fishes[i].energy > 90000.0 {
+                let new = self.fishes[i].split();
+                self.fishes.push(new);
+            }
+        }
+
+        // prevent aquarium leaks
+        let mut i = 0;
+        while i < self.fishes.len() {
+            let fish = &self.fishes[i];
+
+            if fish.x > MAX_X || fish.y > MAX_Y || fish.x < 0.0 || fish.y < 0.0 {
+                self.fishes.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+
+        // Hard coded run-toward smaller and run away from bigger
+        behave_fishes(self, delta_time);
+
+        let fishes = &mut self.fishes;
+        fishes.sort_by(|a, b| (a.energy).partial_cmp(&b.energy).unwrap().reverse());
+
+        // Fishes eat other fishes
+        let mut i = 0;
+        while i < fishes.len() {
+            let mut j = i + 1;
+            while j < fishes.len() {
+                if fishes[i].covers(&fishes[j]) {
+                    let eaten = fishes.remove(j);
+                    fishes[i].eat(&eaten);
+                } else {
+                    j += 1;
+                }
+            }
+            i += 1;
+        }
+    }
+}
+
 pub struct Fish {
     x: f64,
     y: f64,
     energy: f64,
+}
+
+fn behave_fishes(state: &mut State, delta_time: f64) {
+    let fishes = &mut state.fishes;
+    for i in 0..fishes.len() {
+
+        let maybe_j = fishes
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| j != &i)
+            .min_by_key(|(_, fish)| NotNan::from_inner(fishes[i].distance(fish)))
+            .map(|(j, _)| j);
+        
+        if let Some(j) = maybe_j {
+            let mut dv = fishes[i].direction_to(&fishes[j]);
+    
+            if fishes[j].energy >= fishes[i].energy {
+                dv = dv.invert();
+            }
+
+            let speed = 10.0;
+            fishes[i].x += speed * delta_time * dv.x;
+            fishes[i].y += speed * delta_time * dv.y;
+        }
+    }
 }
 
 impl Fish {
@@ -35,19 +170,32 @@ impl Fish {
         Fish { x, y, energy }
     }
 
-    pub fn squared_radius(&self) -> f64 {
-        self.energy / std::f64::consts::PI
+    pub fn radius(&self) -> f64 {
+        (self.energy / std::f64::consts::PI).cbrt()
     }
 
-    pub fn radius(&self) -> f64 {
-        self.squared_radius().sqrt()
+    pub fn surface_area(&self) -> f64 {
+        self.radius().powi(2) * std::f64::consts::PI
+    }
+
+    pub fn distance(&self, other: &Fish) -> f64 {
+        self.displacement_to(other).length()
+    }
+    
+    // Verplaatsingsvector naar. Ha. Blub. Blub. I'm coming to get you.
+    pub fn displacement_to(&self, other: &Fish) -> Vec2 {
+        let dx = other.x - self.x;
+        let dy = other.y - self.y;
+        return Vec2::new(dx, dy); 
+    }
+    
+    pub fn direction_to(&self, other: &Fish) -> Vec2 {
+        let displacement_to = self.displacement_to(other);
+        displacement_to / displacement_to.length()
     }
 
     pub fn covers(&self, other: &Fish) -> bool {
-        let dx = self.x - other.x;
-        let dy = self.y - other.y;
-        let squared_dist = dx.powi(2) + dy.powi(2);
-        self.squared_radius() > squared_dist
+        self.radius() > self.distance(other)
     }
 
     pub fn eat(&mut self, other: &Fish) {
@@ -71,29 +219,10 @@ impl Fish {
         let x_2 = self.x + radius * opposite.cos();
         let y_2 = self.y + radius * opposite.sin();
 
-        let child_energy = (self.energy - 1500.0) / 2.0;
+        let child_energy = self.energy / 2.5;
         self.move_to(x_1, y_1);
         self.energy = child_energy;
         Fish::new(x_2, y_2, child_energy)
-    }
-}
-
-fn step(state: &mut State) {
-    let fishes = &mut state.fishes;
-    fishes.sort_by(|a, b| (a.energy).partial_cmp(&b.energy).unwrap().reverse());
-
-    let mut i = 0;
-    while i < fishes.len() {
-        let mut j = i + 1;
-        while j < fishes.len() {
-            if fishes[i].covers(&fishes[j]) {
-                let eaten = fishes.remove(j);
-                fishes[i].eat(&eaten);
-            } else {
-                j += 1;
-            }
-        }
-        i += 1;
     }
 }
 
@@ -132,30 +261,7 @@ impl App {
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        step(&mut self.state);
-
-        for fish in self.state.fishes.iter_mut() {
-            fish.energy += 300.0 * args.dt;
-        }
-
-        for i in 0..self.state.fishes.len() {
-            if self.state.fishes[i].energy > 9000.0 {
-                let new = self.state.fishes[i].split();
-                self.state.fishes.push(new);
-            }
-        }
-
-        // prevent aquarium leaks
-        let mut i = 0;
-        while i < self.state.fishes.len() {
-            let fish = &self.state.fishes[i];
-
-            if fish.x > MAX_X || fish.y > MAX_Y {
-                self.state.fishes.remove(i);
-            } else {
-                i += 1;
-            }
-        }
+        self.state.update(args.dt);
     }
 }
 
@@ -169,22 +275,11 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
-    let mut fishes = Vec::new();
-    let mut rng = rand::thread_rng();
-
-    for _ in 0..100 {
-        let x = rng.gen_range(0.0..600.0);
-        let y = rng.gen_range(0.0..600.0);
-        let radius = rng.gen_range(5.0..1000.0);
-
-        let fish = Fish::new(x, y, radius);
-        fishes.push(fish);
-    }
 
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        state: State { fishes },
+        state: State::new(),
     };
 
     let mut events = Events::new(EventSettings::new());
